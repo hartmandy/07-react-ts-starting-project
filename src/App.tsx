968f2d3b4f54ed7b81cc10e4a92fab90839eb25b
hartmandy <120344775+hartmandy@users.noreply.github.com>
2023-12-31 17:12:24 -0500

    New todo types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 import "./App.css";
+import NewTodo from "./components/NewTodo";
 
 function App() {
   const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
-
+  const addTodoHandler = (todoText: string) => {};
   return (
     <div className="App">
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
